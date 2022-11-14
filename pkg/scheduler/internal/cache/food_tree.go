fc9a410b060f08a9af64847920fe798c144bd1b2
liuyuchao <blockchain_lyc@163.com>
2022-11-14 12:25:11 +0800

    第五版，增加配置文件说明

---
+++ pkg/scheduler/internal/cache/food_tree.go
@@ -0,0 +1,140 @@
+package cache
+
+import (
+	apidguest "dguest-scheduler/pkg/api/dguest"
+	"dguest-scheduler/pkg/apis/scheduler/v1alpha1"
+	"fmt"
+
+	"k8s.io/klog/v2"
+)
+
+// foodTree is a tree-like data structure that holds food names in each zone. Zone names are
+// keys to "FoodTree.tree" and values of "FoodTree.tree" are arrays of food names.
+// FoodTree is NOT thread-safe, any concurrent updates/reads from it must be synchronized by the caller.
+// It is used only by schedulerCache, and should stay as such.
+type foodTree struct {
+	tree  map[string][]*v1alpha1.FoodInfoBase // a map from zone (region-zone) to an array of foods in the zone.
+	zones []string                            // a list of all the zones in the tree (keys)
+}
+
+// newFoodTree creates a FoodTree from foods.
+func newFoodTree(foods []*v1alpha1.Food) *foodTree {
+	nt := &foodTree{
+		tree: make(map[string][]*v1alpha1.FoodInfoBase),
+	}
+	for _, n := range foods {
+		nt.addFood(n)
+	}
+	return nt
+}
+
+// addFood adds a food and its corresponding zone to the tree. If the zone already exists, the food
+// is added to the array of foods in that zone.
+func (nt *foodTree) addFood(f *v1alpha1.Food) {
+	zone := apidguest.FoodCuisineVersionKey(f)
+	if na, ok := nt.tree[zone]; ok {
+		for _, foodBase := range na {
+			if foodBase.Name == f.Name {
+				klog.InfoS("Food already exists in the FoodTree", "food", klog.KObj(f))
+				return
+			}
+		}
+		nt.tree[zone] = append(na, &v1alpha1.FoodInfoBase{
+			Namespace:      f.Namespace,
+			Name:           f.Name,
+			CuisineVersion: zone,
+		})
+	} else {
+		nt.zones = append(nt.zones, zone)
+		nt.tree[zone] = []*v1alpha1.FoodInfoBase{&v1alpha1.FoodInfoBase{
+			Namespace:      f.Namespace,
+			Name:           f.Name,
+			CuisineVersion: zone,
+		}}
+	}
+	klog.V(2).InfoS("Added food in listed group to FoodTree", "food", klog.KObj(f), "zone", zone)
+}
+
+// removeFood removes a food from the FoodTree.
+func (nt *foodTree) removeFood(n *v1alpha1.Food) error {
+	zone := apidguest.FoodCuisineVersionKey(n)
+	if na, ok := nt.tree[zone]; ok {
+		for i, foodInfo := range na {
+			if foodInfo.Name == n.Name {
+				nt.tree[zone] = append(na[:i], na[i+1:]...)
+				if len(nt.tree[zone]) == 0 {
+					nt.removeZone(zone)
+				}
+				klog.V(2).InfoS("Removed food in listed group from FoodTree", "food", klog.KObj(n), "zone", zone)
+				return nil
+			}
+		}
+	}
+	klog.ErrorS(nil, "Food in listed group was not found", "food", klog.KObj(n), "zone", zone)
+	return fmt.Errorf("food %q in group %q was not found", n.Name, "")
+}
+
+// removeZone removes a zone from tree.
+// This function must be called while writer locks are hold.
+func (nt *foodTree) removeZone(zone string) {
+	delete(nt.tree, zone)
+	for i, z := range nt.zones {
+		if z == zone {
+			nt.zones = append(nt.zones[:i], nt.zones[i+1:]...)
+			return
+		}
+	}
+}
+
+// updateFood updates a food in the FoodTree.
+func (nt *foodTree) updateFood(old, new *v1alpha1.Food) {
+	var oldZone string
+	if old != nil {
+		oldZone = apidguest.FoodCuisineVersionKey(old)
+	}
+	newZone := apidguest.FoodCuisineVersionKey(new)
+	// If the zone ID of the food has not changed, we don't need to do anything. Name of the food
+	// cannot be changed in an update.
+	if oldZone == newZone {
+		return
+	}
+	nt.removeFood(old) // No error checking. We ignore whether the old food exists or not.
+	nt.addFood(new)
+}
+
+func (nt *foodTree) ZoneMap() map[string][]*v1alpha1.FoodInfoBase {
+	return nt.tree
+}
+
+// list returns the list of names of the food. FoodTree iterates over zones and in each zone iterates
+// over foods in a round robin fashion.
+func (nt *foodTree) list(cuisineVersion string) []*v1alpha1.FoodInfoBase {
+	if len(nt.zones) == 0 {
+		return nil
+	}
+	return nt.tree[cuisineVersion]
+	// foodsList := make([]*v1alpha1.FoodInfoBase, 0, nt.numFoods)
+	// numExhaustedZones := 0
+	// foodIndex := 0
+	// for len(foodsList) < nt.numFoods {
+	// 	if numExhaustedZones >= len(nt.zones) { // all zones are exhausted.
+	// 		return foodsList, errors.New("all zones exhausted before reaching count of foods expected")
+	// 	}
+	// 	for zoneIndex := 0; zoneIndex < len(nt.zones); zoneIndex++ {
+	// 		na := nt.tree[nt.zones[zoneIndex]]
+	// 		if foodIndex >= len(na) { // If the zone is exhausted, continue
+	// 			if foodIndex == len(na) { // If it is the first time the zone is exhausted
+	// 				numExhaustedZones++
+	// 			}
+	// 			continue
+	// 		}
+	// 		foodsList = append(foodsList, na[foodIndex])
+	// 	}
+	// 	foodIndex++
+	// }
+	// return foodsList, nil
+}
+
+func (nt *foodTree) foodCount(cuisineVersion string) int {
+	return len(nt.list(cuisineVersion))
+}
---
+++ pkg/scheduler/internal/cache/food_tree_test.go
@@ -0,0 +1,483 @@
+// /*
+// Copyright 2018 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// */
+package cache
+
+//
+//import (
+//	"reflect"
+//	"testing"
+//
+//	v1 "k8s.io/api/core/v1"
+//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+//)
+//
+//var allFoods = []*v1alpha1.Food{
+//	// Food 0: a food without any region-zone label
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-0",
+//		},
+//	},
+//	// Food 1: a food with region label only
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-1",
+//			Labels: map[string]string{
+//				v1.LabelTopologyRegion: "region-1",
+//			},
+//		},
+//	},
+//	// Food 2: a food with zone label only
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-2",
+//			Labels: map[string]string{
+//				v1.LabelTopologyZone: "zone-2",
+//			},
+//		},
+//	},
+//	// Food 3: a food with proper region and zone labels
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-3",
+//			Labels: map[string]string{
+//				v1.LabelTopologyRegion: "region-1",
+//				v1.LabelTopologyZone:   "zone-2",
+//			},
+//		},
+//	},
+//	// Food 4: a food with proper region and zone labels
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-4",
+//			Labels: map[string]string{
+//				v1.LabelTopologyRegion: "region-1",
+//				v1.LabelTopologyZone:   "zone-2",
+//			},
+//		},
+//	},
+//	// Food 5: a food with proper region and zone labels in a different zone, same region as above
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-5",
+//			Labels: map[string]string{
+//				v1.LabelTopologyRegion: "region-1",
+//				v1.LabelTopologyZone:   "zone-3",
+//			},
+//		},
+//	},
+//	// Food 6: a food with proper region and zone labels in a new region and zone
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-6",
+//			Labels: map[string]string{
+//				v1.LabelTopologyRegion: "region-2",
+//				v1.LabelTopologyZone:   "zone-2",
+//			},
+//		},
+//	},
+//	// Food 7: a food with proper region and zone labels in a region and zone as food-6
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-7",
+//			Labels: map[string]string{
+//				v1.LabelTopologyRegion: "region-2",
+//				v1.LabelTopologyZone:   "zone-2",
+//			},
+//		},
+//	},
+//	// Food 8: a food with proper region and zone labels in a region and zone as food-6
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-8",
+//			Labels: map[string]string{
+//				v1.LabelTopologyRegion: "region-2",
+//				v1.LabelTopologyZone:   "zone-2",
+//			},
+//		},
+//	},
+//	// Food 9: a food with zone + region label and the deprecated zone + region label
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-9",
+//			Labels: map[string]string{
+//				v1.LabelTopologyRegion:          "region-2",
+//				v1.LabelTopologyZone:            "zone-2",
+//				v1.LabelFailureDomainBetaRegion: "region-2",
+//				v1.LabelFailureDomainBetaZone:   "zone-2",
+//			},
+//		},
+//	},
+//	// Food 10: a food with only the deprecated zone + region labels
+//	{
+//		ObjectMeta: metav1.ObjectMeta{
+//			Name: "food-10",
+//			Labels: map[string]string{
+//				v1.LabelFailureDomainBetaRegion: "region-2",
+//				v1.LabelFailureDomainBetaZone:   "zone-3",
+//			},
+//		},
+//	},
+//}
+//
+//func verifyFoodTree(t *testing.T, nt *foodTree, expectedTree map[string][]string) {
+//	expectedNumFoods := int(0)
+//	for _, na := range expectedTree {
+//		expectedNumFoods += len(na)
+//	}
+//	if numFoods := nt.numFoods; numFoods != expectedNumFoods {
+//		t.Errorf("unexpected foodTree.numFoods. Expected: %v, Got: %v", expectedNumFoods, numFoods)
+//	}
+//	if !reflect.DeepEqual(nt.tree, expectedTree) {
+//		t.Errorf("The food tree is not the same as expected. Expected: %v, Got: %v", expectedTree, nt.tree)
+//	}
+//	if len(nt.zones) != len(expectedTree) {
+//		t.Errorf("Number of zones in foodTree.zones is not expected. Expected: %v, Got: %v", len(expectedTree), len(nt.zones))
+//	}
+//	for _, z := range nt.zones {
+//		if _, ok := expectedTree[z]; !ok {
+//			t.Errorf("zone %v is not expected to exist in foodTree.zones", z)
+//		}
+//	}
+//}
+//
+//func TestFoodTree_AddFood(t *testing.T) {
+//	tests := []struct {
+//		name         string
+//		foodsToAdd   []*v1alpha1.Food
+//		expectedTree map[string][]string
+//	}{
+//		{
+//			name:         "single food no labels",
+//			foodsToAdd:   allFoods[:1],
+//			expectedTree: map[string][]string{"": {"food-0"}},
+//		},
+//		{
+//			name:       "mix of foods with and without proper labels",
+//			foodsToAdd: allFoods[:4],
+//			expectedTree: map[string][]string{
+//				"":                     {"food-0"},
+//				"region-1:\x00:":       {"food-1"},
+//				":\x00:zone-2":         {"food-2"},
+//				"region-1:\x00:zone-2": {"food-3"},
+//			},
+//		},
+//		{
+//			name:       "mix of foods with and without proper labels and some zones with multiple foods",
+//			foodsToAdd: allFoods[:7],
+//			expectedTree: map[string][]string{
+//				"":                     {"food-0"},
+//				"region-1:\x00:":       {"food-1"},
+//				":\x00:zone-2":         {"food-2"},
+//				"region-1:\x00:zone-2": {"food-3", "food-4"},
+//				"region-1:\x00:zone-3": {"food-5"},
+//				"region-2:\x00:zone-2": {"food-6"},
+//			},
+//		},
+//		{
+//			name:       "foods also using deprecated zone/region label",
+//			foodsToAdd: allFoods[9:],
+//			expectedTree: map[string][]string{
+//				"region-2:\x00:zone-2": {"food-9"},
+//				"region-2:\x00:zone-3": {"food-10"},
+//			},
+//		},
+//	}
+//
+//	for _, test := range tests {
+//		t.Run(test.name, func(t *testing.T) {
+//			nt := newFoodTree(nil)
+//			for _, n := range test.foodsToAdd {
+//				nt.addFood(n)
+//			}
+//			verifyFoodTree(t, nt, test.expectedTree)
+//		})
+//	}
+//}
+//
+//func TestFoodTree_RemoveFood(t *testing.T) {
+//	tests := []struct {
+//		name          string
+//		existingFoods []*v1alpha1.Food
+//		foodsToRemove []*v1alpha1.Food
+//		expectedTree  map[string][]string
+//		expectError   bool
+//	}{
+//		{
+//			name:          "remove a single food with no labels",
+//			existingFoods: allFoods[:7],
+//			foodsToRemove: allFoods[:1],
+//			expectedTree: map[string][]string{
+//				"region-1:\x00:":       {"food-1"},
+//				":\x00:zone-2":         {"food-2"},
+//				"region-1:\x00:zone-2": {"food-3", "food-4"},
+//				"region-1:\x00:zone-3": {"food-5"},
+//				"region-2:\x00:zone-2": {"food-6"},
+//			},
+//		},
+//		{
+//			name:          "remove a few foods including one from a zone with multiple foods",
+//			existingFoods: allFoods[:7],
+//			foodsToRemove: allFoods[1:4],
+//			expectedTree: map[string][]string{
+//				"":                     {"food-0"},
+//				"region-1:\x00:zone-2": {"food-4"},
+//				"region-1:\x00:zone-3": {"food-5"},
+//				"region-2:\x00:zone-2": {"food-6"},
+//			},
+//		},
+//		{
+//			name:          "remove all foods",
+//			existingFoods: allFoods[:7],
+//			foodsToRemove: allFoods[:7],
+//			expectedTree:  map[string][]string{},
+//		},
+//		{
+//			name:          "remove non-existing food",
+//			existingFoods: nil,
+//			foodsToRemove: allFoods[:5],
+//			expectedTree:  map[string][]string{},
+//			expectError:   true,
+//		},
+//	}
+//
+//	for _, test := range tests {
+//		t.Run(test.name, func(t *testing.T) {
+//			nt := newFoodTree(test.existingFoods)
+//			for _, n := range test.foodsToRemove {
+//				err := nt.removeFood(n)
+//				if test.expectError == (err == nil) {
+//					t.Errorf("unexpected returned error value: %v", err)
+//				}
+//			}
+//			verifyFoodTree(t, nt, test.expectedTree)
+//		})
+//	}
+//}
+//
+//func TestFoodTree_UpdateFood(t *testing.T) {
+//	tests := []struct {
+//		name          string
+//		existingFoods []*v1alpha1.Food
+//		foodToUpdate  *v1alpha1.Food
+//		expectedTree  map[string][]string
+//	}{
+//		{
+//			name:          "update a food without label",
+//			existingFoods: allFoods[:7],
+//			foodToUpdate: &v1alpha1.Food{
+//				ObjectMeta: metav1.ObjectMeta{
+//					Name: "food-0",
+//					Labels: map[string]string{
+//						v1.LabelTopologyRegion: "region-1",
+//						v1.LabelTopologyZone:   "zone-2",
+//					},
+//				},
+//			},
+//			expectedTree: map[string][]string{
+//				"region-1:\x00:":       {"food-1"},
+//				":\x00:zone-2":         {"food-2"},
+//				"region-1:\x00:zone-2": {"food-3", "food-4", "food-0"},
+//				"region-1:\x00:zone-3": {"food-5"},
+//				"region-2:\x00:zone-2": {"food-6"},
+//			},
+//		},
+//		{
+//			name:          "update the only existing food",
+//			existingFoods: allFoods[:1],
+//			foodToUpdate: &v1alpha1.Food{
+//				ObjectMeta: metav1.ObjectMeta{
+//					Name: "food-0",
+//					Labels: map[string]string{
+//						v1.LabelTopologyRegion: "region-1",
+//						v1.LabelTopologyZone:   "zone-2",
+//					},
+//				},
+//			},
+//			expectedTree: map[string][]string{
+//				"region-1:\x00:zone-2": {"food-0"},
+//			},
+//		},
+//		{
+//			name:          "update non-existing food",
+//			existingFoods: allFoods[:1],
+//			foodToUpdate: &v1alpha1.Food{
+//				ObjectMeta: metav1.ObjectMeta{
+//					Name: "food-new",
+//					Labels: map[string]string{
+//						v1.LabelTopologyRegion: "region-1",
+//						v1.LabelTopologyZone:   "zone-2",
+//					},
+//				},
+//			},
+//			expectedTree: map[string][]string{
+//				"":                     {"food-0"},
+//				"region-1:\x00:zone-2": {"food-new"},
+//			},
+//		},
+//	}
+//
+//	for _, test := range tests {
+//		t.Run(test.name, func(t *testing.T) {
+//			nt := newFoodTree(test.existingFoods)
+//			var oldFood *v1alpha1.Food
+//			for _, n := range allFoods {
+//				if n.Name == test.foodToUpdate.Name {
+//					oldFood = n
+//					break
+//				}
+//			}
+//			if oldFood == nil {
+//				oldFood = &v1alpha1.Food{ObjectMeta: metav1.ObjectMeta{Name: "nonexisting-food"}}
+//			}
+//			nt.updateFood(oldFood, test.foodToUpdate)
+//			verifyFoodTree(t, nt, test.expectedTree)
+//		})
+//	}
+//}
+//
+//func TestFoodTree_List(t *testing.T) {
+//	tests := []struct {
+//		name           string
+//		foodsToAdd     []*v1alpha1.Food
+//		expectedOutput []string
+//	}{
+//		{
+//			name:           "empty tree",
+//			foodsToAdd:     nil,
+//			expectedOutput: nil,
+//		},
+//		{
+//			name:           "one food",
+//			foodsToAdd:     allFoods[:1],
+//			expectedOutput: []string{"food-0"},
+//		},
+//		{
+//			name:           "four foods",
+//			foodsToAdd:     allFoods[:4],
+//			expectedOutput: []string{"food-0", "food-1", "food-2", "food-3"},
+//		},
+//		{
+//			name:           "all foods",
+//			foodsToAdd:     allFoods[:9],
+//			expectedOutput: []string{"food-0", "food-1", "food-2", "food-3", "food-5", "food-6", "food-4", "food-7", "food-8"},
+//		},
+//	}
+//
+//	for _, test := range tests {
+//		t.Run(test.name, func(t *testing.T) {
+//			nt := newFoodTree(test.foodsToAdd)
+//
+//			output, err := nt.list()
+//			if err != nil {
+//				t.Fatal(err)
+//			}
+//			if !reflect.DeepEqual(output, test.expectedOutput) {
+//				t.Errorf("unexpected output. Expected: %v, Got: %v", test.expectedOutput, output)
+//			}
+//		})
+//	}
+//}
+//
+//func TestFoodTree_List_Exhausted(t *testing.T) {
+//	nt := newFoodTree(allFoods[:9])
+//	nt.numFoods++
+//	_, err := nt.list()
+//	if err == nil {
+//		t.Fatal("Expected an error from zone exhaustion")
+//	}
+//}
+//
+//func TestFoodTreeMultiOperations(t *testing.T) {
+//	tests := []struct {
+//		name           string
+//		foodsToAdd     []*v1alpha1.Food
+//		foodsToRemove  []*v1alpha1.Food
+//		operations     []string
+//		expectedOutput []string
+//	}{
+//		{
+//			name:           "add and remove all foods",
+//			foodsToAdd:     allFoods[2:9],
+//			foodsToRemove:  allFoods[2:9],
+//			operations:     []string{"add", "add", "add", "remove", "remove", "remove"},
+//			expectedOutput: nil,
+//		},
+//		{
+//			name:           "add and remove some foods",
+//			foodsToAdd:     allFoods[2:9],
+//			foodsToRemove:  allFoods[2:9],
+//			operations:     []string{"add", "add", "add", "remove"},
+//			expectedOutput: []string{"food-3", "food-4"},
+//		},
+//		{
+//			name:           "remove three foods",
+//			foodsToAdd:     allFoods[2:9],
+//			foodsToRemove:  allFoods[2:9],
+//			operations:     []string{"add", "add", "add", "remove", "remove", "remove", "add"},
+//			expectedOutput: []string{"food-5"},
+//		},
+//		{
+//			name:           "add more foods to an exhausted zone",
+//			foodsToAdd:     append(allFoods[4:9:9], allFoods[3]),
+//			foodsToRemove:  nil,
+//			operations:     []string{"add", "add", "add", "add", "add", "add"},
+//			expectedOutput: []string{"food-4", "food-5", "food-6", "food-3", "food-7", "food-8"},
+//		},
+//		{
+//			name:           "remove zone and add new",
+//			foodsToAdd:     append(allFoods[3:5:5], allFoods[6:8]...),
+//			foodsToRemove:  allFoods[3:5],
+//			operations:     []string{"add", "add", "remove", "add", "add", "remove"},
+//			expectedOutput: []string{"food-6", "food-7"},
+//		},
+//	}
+//
+//	for _, test := range tests {
+//		t.Run(test.name, func(t *testing.T) {
+//			nt := newFoodTree(nil)
+//			addIndex := 0
+//			removeIndex := 0
+//			for _, op := range test.operations {
+//				switch op {
+//				case "add":
+//					if addIndex >= len(test.foodsToAdd) {
+//						t.Error("more add operations than foodsToAdd")
+//					} else {
+//						nt.addFood(test.foodsToAdd[addIndex])
+//						addIndex++
+//					}
+//				case "remove":
+//					if removeIndex >= len(test.foodsToRemove) {
+//						t.Error("more remove operations than foodsToRemove")
+//					} else {
+//						nt.removeFood(test.foodsToRemove[removeIndex])
+//						removeIndex++
+//					}
+//				default:
+//					t.Errorf("unknown operation: %v", op)
+//				}
+//			}
+//			output, err := nt.list()
+//			if err != nil {
+//				t.Fatal(err)
+//			}
+//			if !reflect.DeepEqual(output, test.expectedOutput) {
+//				t.Errorf("unexpected output. Expected: %v, Got: %v", test.expectedOutput, output)
+//			}
+//		})
+//	}
+//}
